74cd41cd913038fba757fa51ede81c71c0f445c4
Jesper Ekerling <jesper@forme.se>
2023-12-11 11:43:04 +0100

    Javascript testing

---
+++ index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+
+
+const birthYear = [1975, 1997, 2002, 1995, 1985]
+
+
+//const ages = _.map(birthYear, (year) => 2023 - year)
+//console.log(ages)
+
+const driverLicense = _.map(birthYear, (licenseYear) => licenseYear + 18)
+
+console.log(driverLicense)
