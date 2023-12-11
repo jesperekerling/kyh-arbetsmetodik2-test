b08ecaf9d3abea9d05915be86feefbc01cac7833
Jesper Ekerling <jesper@forme.se>
2023-12-11 11:51:33 +0100

    Test

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ const birthYear = [1975, 1997, 2002, 1995, 1985]
 
 const driverLicense = _.map(birthYear, (licenseYear) => licenseYear + 18)
 
-console.log(driverLicense)
+console.log(driverLicense);
